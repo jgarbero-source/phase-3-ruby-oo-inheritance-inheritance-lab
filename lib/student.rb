9f615504b8ec61ed927248809b8c6d9ba00f02a5
John Garbero <john.garbero.itb@gmail.com>
2022-06-30 16:06:03 -0500

    passed tests

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require 'pry'
+
+class Student < User
+
+    attr_accessor :knowledge, :string
+
+    def initialize
+        @knowledge= []
+    end
+
+    def learn(string)
+        @string=string
+        @knowledge << @string
+    end
+
+    binding.pry
 
 end
